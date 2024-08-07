ea463bf5fc331269fe7fba649367c3993f767cf0
Bharghavkrishnakancharla <bharghava.kancharla@gmail.com>
2024-08-07 19:27:49 +0530

    add

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const inputBox = document.querySelector(".input-box");
 const listItems = document.querySelector(".list-items");
- function addTask(){
-    if(inputBox && inputBox.value === ''){
-        alert("you must write something");
-    }
-    else{
+
+function addTask() {
+    if (inputBox && inputBox.value === '') {
+        alert("You must write something");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listItems.appendChild(li);
@@ -14,20 +14,36 @@ const listItems = document.querySelector(".list-items");
     }
     inputBox.value = '';
     saveData();
- }
- listItems.addEventListener("click",function(e){
-    if(e.target.tagName === 'LI'){
+}
+
+listItems.addEventListener("click", function(e) {
+    if (e.target.tagName === 'LI') {
         e.target.classList.toggle("checked");
         saveData();
-    }else if(e.target.tagName === "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        savaData();
+        saveData();
     }
- })
- function saveData(){
-    localStorage.setItem("data",listItems.innerHTML);
- }
- function showTask(){
+});
+
+function saveData() {
+    const items = Array.from(listItems.children);
+    items.sort((a, b) => {
+        if (a.classList.contains("checked") && !b.classList.contains("checked")) {
+            return 1;
+        } else if (!a.classList.contains("checked") && b.classList.contains("checked")) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    listItems.innerHTML = '';
+    items.forEach(item => listItems.appendChild(item));
+    localStorage.setItem("data", listItems.innerHTML);
+}
+
+function showTask() {
     listItems.innerHTML = localStorage.getItem("data");
- }
- showTask();
+}
+
+showTask();
